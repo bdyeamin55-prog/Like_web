52430da618b40aa75099dcfb401a107951cc3ea7
bdyeamin55-prog <bdyeamin55@gmail.com>
2026-04-14 07:38:54 +0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 from app import app
+import os
 
 if __name__ == "__main__":
-    app.run()
+    # Render বা অন্য প্ল্যাটফর্মের জন্য ডাইনামিক পোর্ট সেট করা
+    port = int(os.environ.get("PORT", 31208))
+    app.run(host='0.0.0.0', port=port)
